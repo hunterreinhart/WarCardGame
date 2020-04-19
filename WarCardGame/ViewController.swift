ae568d26279ecd11fa3dbfc760163f06d569e086
hunterreinhart <jreinhar@usc.edu>
2020-04-19 14:22:10 -0400

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Hunter Reinhart on 4/19/20.
+//  Copyright © 2020 Hunter Reinhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
